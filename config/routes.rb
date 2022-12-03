1d5ef221511f04e66f812953aef7a135451b07ab
kta-pink <kta.firstuse@gmail.com>
2022-12-03 08:25:50 +0000

    3.4 repair

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "staic_pages#home"
-  get  "static_pages/home"
-  get  "static_pages/help"
-  get  "static_pages/about"
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
