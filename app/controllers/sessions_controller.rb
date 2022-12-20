7911be6f4049161d3ab5879cb8ea17607baf3463
kta-pink <kta.firstuse@gmail.com>
2022-12-20 08:44:53 +0000

    9.4 Implement advanced login Finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session      # ハイジャック対策でログインの直前に必ずこれを書くこと
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,12 +25,12 @@ module SessionsHelper
     end
   end
 
-  # 現在ログイン中のユーザーを返す（いる場合）
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
+  # # 現在ログイン中のユーザーを返す（いる場合）
+  # def current_user
+  #   if session[:user_id]
+  #     @current_user ||= User.find_by(id: session[:user_id])
+  #   end
+  # end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
