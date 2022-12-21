70f3f58530c4514b85cdf15d4e230d688896cef5
kta-pink <kta.firstuse@gmail.com>
2022-12-21 08:03:19 +0000

    9.2 Finiehd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  #edit,updateのみ事前ログインをさせる
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,10 +24,42 @@ class UsersController < ApplicationController
     end
   end
 
+   def edit
+    #@user = User.find(params[:id])
+   end
+
+   def update
+    #@user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+   end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # beforeフィルタ
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 end
